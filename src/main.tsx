99346ce60762a0e1df6bfd2f1fd73254d18057f0
devbyte <demiladeakinyemi2004@gmail.com>
2026-07-04 19:08:55 -0400

    refactor: remove Web3Provider wrapper and import from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,14 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import { ThemeProvider } from './contexts/ThemeContext';
-import { Web3Provider } from './providers/Web3Provider';
 import { WalletProvider } from './providers/WalletProvider';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
-      <Web3Provider>
-        <WalletProvider>
-          <App />
-        </WalletProvider>
-      </Web3Provider>
+      <WalletProvider>
+        <App />
+      </WalletProvider>
     </ThemeProvider>
   </StrictMode>
 );
